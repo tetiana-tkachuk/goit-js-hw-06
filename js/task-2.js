7c11a4c30077ac3ec6074daa43070f4c156e05fd
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-11-30 12:50:17 +0200

    Add initial js files

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+// class Storage {}
+
+// const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// storage.removeItem('Scaner');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+// class StringBuilder {}
+
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // "."
+// builder.padStart('^');
+// console.log(builder.getValue()); // "^."
+// builder.padEnd('^');
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth('=');
+// console.log(builder.getValue()); // "=^.^="
