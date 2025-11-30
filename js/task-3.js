691034c3e9059c4d5359f86b64c4094922f6417f
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-11-30 14:36:32 +0200

    Complete task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,32 @@
-// class StringBuilder {}
+class StringBuilder {
+  #value;
 
-// const builder = new StringBuilder('.');
-// console.log(builder.getValue()); // "."
-// builder.padStart('^');
-// console.log(builder.getValue()); // "^."
-// builder.padEnd('^');
-// console.log(builder.getValue()); // "^.^"
-// builder.padBoth('=');
-// console.log(builder.getValue()); // "=^.^="
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
